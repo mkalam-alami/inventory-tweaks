91e01805384820add0ccebe5d6423a88d8b4b48f
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2012-01-31 15:45:28 +0100

    Update changelog/license, 1.73c

--- src/InvTweaksConst.java
+++ src/InvTweaksConst.java
@@ -6,7 +6,7 @@ import org.lwjgl.input.Keyboard;
 public class InvTweaksConst {
 
     // Mod version
-    public static final String MOD_VERSION = "1.37b (1.1)";
+    public static final String MOD_VERSION = "1.37c (1.1)";
 
     // Mod tree version
     // Change only when the tree evolves significantly enough to need to override all configs

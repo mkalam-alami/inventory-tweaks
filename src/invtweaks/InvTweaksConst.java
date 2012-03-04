f2d080cf6cdc79299945e42a34cb276e0f969c17
Marwane Kalam-Alami <marwane.ka@gmail.com>
2012-03-04 12:17:57 +0100

    Bump version to 1.2.3

--- src/invtweaks/InvTweaksConst.java
+++ src/invtweaks/InvTweaksConst.java
@@ -8,7 +8,7 @@ import net.minecraft.client.Minecraft;
 public class InvTweaksConst {
 
     // Mod version
-    public static final String MOD_VERSION = "1.40 (1.1)";
+    public static final String MOD_VERSION = "1.40 (1.2.3)";
 
     // Mod tree version
     // Change only when the tree evolves significantly enough to need to override all configs

caaa87f5f4c3214cecbda7b485bbff733f61e0a8
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2012-08-01 16:55:12 +0200

    Add missing file

---
+++ src/EntityRendererProxy_InvTweaks.java
@@ -0,0 +1,19 @@
+import net.minecraft.client.Minecraft;
+
+public class EntityRendererProxy_InvTweaks extends auv {
+    private Minecraft game;
+
+    public EntityRendererProxy_InvTweaks(Minecraft minecraft) {
+        super(minecraft);
+        this.game = minecraft;
+    }
+
+    public void b(float tick) {
+        super.b(tick);
+        try {
+            ModLoader_InvTweaks.onTick(tick, this.game);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

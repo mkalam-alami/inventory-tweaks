97f78c7a18a455f90ece29f8afb219ce053e6b23
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2012-08-07 12:58:19 +0200

    Allow to set sort key from the '...' menu

--- src/InvTweaksGuiSettings.java
+++ src/InvTweaksGuiSettings.java
@@ -28,6 +28,7 @@ public class InvTweaksGuiSettings extends InvTweaksGuiSettingsAbstract {
     private final static int ID_SHORTCUTS_HELP = 4;
     private final static int ID_AUTO_REFILL = 5;
     private final static int ID_MORE_OPTIONS = 6;
+    private final static int ID_SORTING_KEY = 7;
     private final static int ID_EDITRULES = 100;
     private final static int ID_EDITTREE = 101;
     private final static int ID_HELP = 102;
@@ -37,6 +38,10 @@ public class InvTweaksGuiSettings extends InvTweaksGuiSettingsAbstract {
     private static String labelShortcuts;
     private static String labelAutoRefill;
     private static String labelMoreOptions;
+
+    private InvTweaksGuiTooltipButton sortMappingButton;
+    private boolean sortMappingEdition = false;
+    private int sortMappingId;
     
     public InvTweaksGuiSettings(Minecraft mc, apm parentScreen, InvTweaksConfig config) {
         super(mc, parentScreen, config);
@@ -77,6 +82,12 @@ public class InvTweaksGuiSettings extends InvTweaksGuiSettingsAbstract {
         }
 
         moveToButtonCoords(i++, p);
+        sortMappingId = getSortMappingId();
+        sortMappingButton = new InvTweaksGuiTooltipButton(ID_SORTING_KEY, p.getX(), p.getY(),
+                InvTweaksLocalization.get("invtweaks.settings.key") + " " + obf.getGameSettings().b(sortMappingId));
+        controlList.add(sortMappingButton);
+        
+        moveToButtonCoords(i++, p);
         controlList.add(new InvTweaksGuiTooltipButton(ID_SHORTCUTS_HELP, 
                 p.getX() + 130, p.getY(), 20, 20, "?", "Shortcuts help"));
         String shortcuts = config.getProperty(InvTweaksConfig.PROP_ENABLE_SHORTCUTS);
@@ -93,15 +104,13 @@ public class InvTweaksGuiSettings extends InvTweaksGuiSettingsAbstract {
         InvTweaksGuiTooltipButton autoRefillBtn = new InvTweaksGuiTooltipButton(ID_AUTO_REFILL, p.getX(), p.getY(), computeBooleanButtonLabel(
                 InvTweaksConfig.PROP_ENABLE_AUTO_REFILL, labelAutoRefill), InvTweaksLocalization.get("invtweaks.settings.autorefill.tooltip"));
         controlList.add(autoRefillBtn);
-        
-        moveToButtonCoords(i++, p);
-        controlList.add(new InvTweaksGuiTooltipButton(ID_CHESTS_BUTTONS, p.getX(), p.getY(), computeBooleanButtonLabel(
-                InvTweaksConfig.PROP_SHOW_CHEST_BUTTONS, labelChestButtons), InvTweaksLocalization.get("invtweaks.settings.chestbuttons.tooltip")));
 
-        moveToButtonCoords(i++, p);
         moveToButtonCoords(i++, p);
         controlList.add(new InvTweaksGuiTooltipButton(ID_MORE_OPTIONS, p.getX(), p.getY(), labelMoreOptions, InvTweaksLocalization.get("invtweaks.settings.moreoptions.tooltip")));
     
+        moveToButtonCoords(i++, p);
+        controlList.add(new InvTweaksGuiTooltipButton(ID_CHESTS_BUTTONS, p.getX(), p.getY(), computeBooleanButtonLabel(
+                InvTweaksConfig.PROP_SHOW_CHEST_BUTTONS, labelChestButtons), InvTweaksLocalization.get("invtweaks.settings.chestbuttons.tooltip")));
         
         // Check if links to files are supported, if not disable the buttons
         if (!Desktop.isDesktopSupported()) {
@@ -119,11 +128,17 @@ public class InvTweaksGuiSettings extends InvTweaksGuiSettingsAbstract {
         obf.setControlList(this, controlList);
 
     }
-
+    
     protected void a(aog guibutton) { /* actionPerformed */
     	super.a(guibutton);
     
         switch (obf.getId(guibutton)) {
+
+        // Switch sorting key
+        case ID_SORTING_KEY:
+            sortMappingButton.e = InvTweaksLocalization.get("invtweaks.settings.key") + " > ??? <";
+            sortMappingEdition = true;
+            break;
 
         // Toggle middle click shortcut
         case ID_MIDDLE_CLICK:
@@ -185,5 +200,25 @@ public class InvTweaksGuiSettings extends InvTweaksGuiSettingsAbstract {
         }
 
     }
+
+    protected void a(char c, int keyCode) { /* keyPressed */
+        if (sortMappingEdition) {
+           obf.getGameSettings().a(sortMappingId, keyCode);
+           sortMappingButton.e = InvTweaksLocalization.get("invtweaks.settings.key") + " " + obf.getGameSettings().b(sortMappingId);
+        }
+        
+    }
+
+    private int getSortMappingId() {
+        int sortMappingId = 0;
+        for (ane mapping : obf.getRegisteredBindings()) {
+            if (mapping == InvTweaks.SORT_KEY_BINDING) {
+                break;
+            }
+            sortMappingId++;
+        }
+        return (sortMappingId == obf.getRegisteredBindings().length) ? -1 : sortMappingId;
+    }
+
 
 }

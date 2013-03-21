2a9c6fbe04548be5bf18605db33bda8206d940ff
Kobata <kobata2@gmail.com>
2013-03-21 13:14:50 -0400

    Comment documenting what the various action values do.

--- src/minecraft/invtweaks/forge/CommonProxy.java
+++ src/minecraft/invtweaks/forge/CommonProxy.java
@@ -22,6 +22,15 @@ public class CommonProxy {
     public void setServerHasInvTweaks(boolean hasInvTweaks) {
     }
 
+    /* Action values:
+     * 0: Standard Click
+     * 1: Shift-Click
+     * 2: Move item to/from hotbar slot (Depends on current slot and hotbar slot being full or empty)
+     * 3: Pick-Block
+     * 4: Drop item
+     * 5: Spread items (Drag behavior)
+     * 6: Merge all valid items with held item
+     */
     public void slotClick(PlayerControllerMP playerController,
                           int windowId, int slot, int data,
                           int action, EntityPlayer player) {

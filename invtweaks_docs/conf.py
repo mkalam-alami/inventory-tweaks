e7ea5eb83baf9909de9f4461cca36aa01de32f1c
Kobata <kobata2@gmail.com>
2013-05-01 19:11:04 -0400

    Version change for dev.

--- invtweaks_docs/conf.py
+++ invtweaks_docs/conf.py
@@ -21,7 +21,7 @@ import sys, os
 # -- General configuration -----------------------------------------------------
 
 # INVENTORY TWEAKS VERSION
-invtweaks_version = '1.54'
+invtweaks_version = '1.55-dev'
 
 
 

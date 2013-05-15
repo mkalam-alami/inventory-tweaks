efa67f697ff616eeeb698b5cf0954940bd542d71
Kobata <kobata2@gmail.com>
2013-05-15 01:10:55 -0400

    Add remapping access transformer.
    Now you can write runtime access transformers that use the SRG names of things!

--- src/minecraft/invtweaks/forge/asm/ITAccessTransformer.java
+++ src/minecraft/invtweaks/forge/asm/ITAccessTransformer.java
@@ -1,11 +1,7 @@
 package invtweaks.forge.asm;
 
-import cpw.mods.fml.common.asm.transformers.AccessTransformer;
-
-import java.io.IOException;
-
-public class ITAccessTransformer extends AccessTransformer {
-    public ITAccessTransformer() throws IOException {
-        super("invtweaks_at.cfg");
+public class ITAccessTransformer extends RemappingAccessTransformer {
+    public ITAccessTransformer() {
+        readFile("invtweaks_at.cfg");
     }
 }

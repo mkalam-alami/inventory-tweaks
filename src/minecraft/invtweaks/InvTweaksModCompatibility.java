26b3fe38299c0594a769e849f12c4fd9d5bc7e55
Kobata <kobata2@gmail.com>
2013-05-17 04:55:28 -0400

    Remove old API annotations as the transformer doesn't reference them.
    The rest of ModCompatiblity is effectively unused, but is waiting for being fully integrated into the transformer.

--- src/minecraft/invtweaks/InvTweaksModCompatibility.java
+++ src/minecraft/invtweaks/InvTweaksModCompatibility.java
@@ -1,10 +1,7 @@
 package invtweaks;
 
-import invtweaks.api.ContainerGUI;
 import invtweaks.api.ContainerSection;
-import invtweaks.api.InventoryGUI;
 import invtweaks.api.container.ChestContainer;
-import invtweaks.api.container.ContainerSectionCallback;
 import invtweaks.api.container.InventoryContainer;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
@@ -35,9 +32,7 @@ public class InvTweaksModCompatibility {
      * @param guiScreen
      */
     public boolean isSpecialChest(GuiScreen guiScreen, Container container) {
-        return getChestContainerAnnotation(container.getClass()) != null // API-marked container classes
-                || getContainerGUIAnnotation(guiScreen.getClass()) != null // API-marked GUI classes
-                || isExact(guiScreen, "cpw.mods.ironchest.client.GUIChest") // Iron chests (formerly IC2)
+        return isExact(guiScreen, "cpw.mods.ironchest.client.GUIChest") // Iron chests (formerly IC2)
                 || isExact(guiScreen, "cubex2.mods.multipagechest.client.GuiMultiPageChest") // Multi Page chest
                 || isExact(guiScreen, "com.eloraam.redpower.machine.GuiBufferChest") // Red Power 2
                 || isExact(guiScreen, "com.eloraam.redpower.machine.GuiRetriever") // Red Power 2
@@ -71,37 +66,7 @@ public class InvTweaksModCompatibility {
      * @param defaultValue
      */
     public int getSpecialChestRowSize(GuiContainer guiContainer, Container container, int defaultValue) {
-        ChestContainer containerAnnotation = getChestContainerAnnotation(container.getClass());
-        if(containerAnnotation != null) {
-            Method m = getAnnotatedMethod(guiContainer.getClass(), new Class[]{ChestContainer.RowSizeCallback.class}, 0,
-                                          int.class);
-            if(m != null) {
-                try {
-                    return (Integer) m.invoke(guiContainer);
-                } catch(Exception e) {
-                    // TODO: Do something here to tell mod authors they're doing it wrong.
-                    return containerAnnotation.rowSize();
-                }
-            } else {
-                return containerAnnotation.rowSize();
-            }
-        }
-
-        ContainerGUI annotation = getContainerGUIAnnotation(guiContainer.getClass());
-        if(annotation != null) {
-            Method m = getAnnotatedMethod(guiContainer.getClass(), new Class[]{ContainerGUI.RowSizeCallback.class}, 0,
-                                          int.class);
-            if(m != null) {
-                try {
-                    return (Integer) m.invoke(guiContainer);
-                } catch(Exception e) {
-                    // TODO: Do something here to tell mod authors they're doing it wrong.
-                    return annotation.rowSize();
-                }
-            } else {
-                return annotation.rowSize();
-            }
-        } else if(isExact(guiContainer, "cpw.mods.ironchest.client.GUIChest")) { // Iron chests (formerly IC2)
+        if(isExact(guiContainer, "cpw.mods.ironchest.client.GUIChest")) { // Iron chests (formerly IC2)
             try {
                 return (Integer) guiContainer.getClass().getMethod("getRowLength").invoke(guiContainer);
             } catch(Exception e) {
@@ -145,17 +110,7 @@ public class InvTweaksModCompatibility {
      * @param guiScreen
      */
     public boolean isSpecialInventory(GuiScreen guiScreen) {
-        // TODO: Make container an argument
-        if(guiScreen instanceof GuiContainer) {
-            InventoryContainer annotation =
-                    getInventoryContainerAnnotation(obf.getContainer(obf.asGuiContainer(guiScreen)).getClass());
-            if(annotation != null && !annotation.showOptions()) {
-                return true;
-            }
-        }
-        if(getInventoryGUIAnnotation(guiScreen.getClass()) != null) {
-            return true;
-        } else if(isExact(guiScreen, "micdoodle8.mods.galacticraft.core.client.gui.GCCoreGuiTankRefill")) {
+        if(isExact(guiScreen, "micdoodle8.mods.galacticraft.core.client.gui.GCCoreGuiTankRefill")) {
             return true;
         }
         try {
@@ -172,14 +127,6 @@ public class InvTweaksModCompatibility {
      * @param guiScreen
      */
     public boolean isStandardInventory(GuiScreen guiScreen) {
-        // TODO: Make container an argument
-        if(guiScreen instanceof GuiContainer) {
-            InventoryContainer annotation =
-                    getInventoryContainerAnnotation(obf.getContainer(obf.asGuiContainer(guiScreen)).getClass());
-            if(annotation != null && annotation.showOptions()) {
-                return true;
-            }
-        }
         if(isExact(guiScreen, "micdoodle8.mods.galacticraft.core.client.gui.GCCoreGuiInventory")) {
             return true;
         }
@@ -188,37 +135,6 @@ public class InvTweaksModCompatibility {
 
     @SuppressWarnings("unchecked")
     public Map<ContainerSection, List<Slot>> getSpecialContainerSlots(GuiScreen guiScreen, Container container) {
-        if(container != null) {
-            Class<? extends Container> clazz = container.getClass();
-            if(isAPIContainer(clazz)) {
-                Method m = getAnnotatedMethod(clazz, new Class[]{ContainerSectionCallback.class}, 0, Map.class);
-                if(m != null) {
-                    try {
-                        return (Map<ContainerSection, List<Slot>>) m.invoke(guiScreen);
-                    } catch(Exception e) {
-                        // TODO: Do something here to tell mod authors they're doing it wrong.
-                    }
-                }
-            }
-        }
-
-        // TODO: Remove with ContainerGUI/InventoryGUI in MC1.6
-        if(guiScreen != null) {
-            Class<? extends GuiScreen> clazz = guiScreen.getClass();
-            if(isAPIClass(clazz)) {
-                Method m = getAnnotatedMethod(clazz, new Class[]{ContainerGUI.ContainerSectionCallback.class,
-                                                                 InventoryGUI.ContainerSectionCallback.class}, 0,
-                                              Map.class);
-                if(m != null) {
-                    try {
-                        return (Map<ContainerSection, List<Slot>>) m.invoke(guiScreen);
-                    } catch(Exception e) {
-                        // TODO: Do something here to tell mod authors they're doing it wrong.
-                    }
-                }
-            }
-        }
-
         Map<ContainerSection, List<Slot>> result = new HashMap<ContainerSection, List<Slot>>();
         List<Slot> slots = (List<Slot>) obf.getSlots(container);
 
@@ -261,16 +177,6 @@ public class InvTweaksModCompatibility {
         }
     }
 
-    @Deprecated
-    private static ContainerGUI getContainerGUIAnnotation(Class<? extends GuiScreen> clazz) {
-        return clazz.getAnnotation(ContainerGUI.class);
-    }
-
-    @Deprecated
-    private static InventoryGUI getInventoryGUIAnnotation(Class<? extends GuiScreen> clazz) {
-        return clazz.getAnnotation(InventoryGUI.class);
-    }
-
     private static InventoryContainer getInventoryContainerAnnotation(Class<? extends Container> clazz) {
         return clazz.getAnnotation(InventoryContainer.class);
     }
@@ -278,11 +184,6 @@ public class InvTweaksModCompatibility {
     private static ChestContainer getChestContainerAnnotation(Class<? extends Container> clazz) {
         return clazz.getAnnotation(ChestContainer.class);
 
-    }
-
-    @Deprecated
-    private static boolean isAPIClass(Class<? extends GuiScreen> clazz) {
-        return (getContainerGUIAnnotation(clazz) != null) || (getInventoryGUIAnnotation(clazz) != null);
     }
 
     private static boolean isAPIContainer(Class<? extends Container> clazz) {

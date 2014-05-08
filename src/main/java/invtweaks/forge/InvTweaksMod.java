dd762f02e27d9b0ab93279940b16dfaee2dd6904
Kobata <kobata2@gmail.com>
2014-05-08 06:02:46 -0400

    Bump FML/Forge version requirements.
    FML changed a bunch early on in 1.7, and I still had the 1.6 values there.

--- src/main/java/invtweaks/forge/InvTweaksMod.java
+++ src/main/java/invtweaks/forge/InvTweaksMod.java
@@ -18,7 +18,7 @@ import net.minecraft.item.ItemStack;
  *         Source code: <a href="https://github.com/kobata/inventory-tweaks">GitHub</a> License: MIT
  */
 @Mod(modid = "inventorytweaks",
-     dependencies = "required-after:FML@[6.2.0,);required-after:Forge@[9.10.0,)")
+     dependencies = "required-after:FML@[7.2.0,);required-after:Forge@[10.12.1,)")
 public class InvTweaksMod implements InvTweaksAPI {
     @Mod.Instance
     public static InvTweaksMod instance;

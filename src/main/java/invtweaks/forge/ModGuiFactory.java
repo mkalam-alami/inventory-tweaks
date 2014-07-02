7d397cba0fc235f52bb0537acdf4cb0f5b34fc3a
Kobata <kobata2@gmail.com>
2014-07-02 19:03:12 -0400

    Apparently I forgot to add the actual ModGuiFactory file.

---
+++ src/main/java/invtweaks/forge/ModGuiFactory.java
@@ -0,0 +1,30 @@
+package invtweaks.forge;
+
+import cpw.mods.fml.client.IModGuiFactory;
+import invtweaks.InvTweaksGuiSettings;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+
+import java.util.Set;
+
+public class ModGuiFactory implements IModGuiFactory {
+    @Override
+    public void initialize(Minecraft minecraftInstance) {
+
+    }
+
+    @Override
+    public Class<? extends GuiScreen> mainConfigGuiClass() {
+        return InvTweaksGuiSettings.class;
+    }
+
+    @Override
+    public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
+        return null;
+    }
+
+    @Override
+    public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element) {
+        return null;
+    }
+}

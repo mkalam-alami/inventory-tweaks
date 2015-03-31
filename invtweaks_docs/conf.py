9732a3e7cf41ecbc52cd8fe18fb7421d0480dd3c
Kobata <kobata2@gmail.com>
2015-03-31 10:34:08 -0400

    Update version to 1.60-dev

--- invtweaks_docs/conf.py
+++ invtweaks_docs/conf.py
@@ -21,7 +21,7 @@ import sys, os
 # -- General configuration -----------------------------------------------------
 
 # INVENTORY TWEAKS VERSION
-invtweaks_version = '1.59'
+invtweaks_version = '1.560-dev'
 
 
 

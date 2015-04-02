2e6c8c6189871fd62e0fda287ad2357b14e2ea87
Kobata <kobata2@gmail.com>
2015-04-02 11:12:27 -0400

    Remeber to mark handler as sharable so it works.

--- src/main/java/invtweaks/network/ITPacketHandler.java
+++ src/main/java/invtweaks/network/ITPacketHandler.java
@@ -2,11 +2,13 @@ package invtweaks.network;
 
 import invtweaks.forge.InvTweaksMod;
 import invtweaks.network.packets.ITPacket;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import net.minecraft.network.INetHandler;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 
+@ChannelHandler.Sharable
 public class ITPacketHandler extends SimpleChannelInboundHandler<ITPacket> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, ITPacket msg) throws Exception {
